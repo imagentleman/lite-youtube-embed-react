8e4877ce71e26f01407c101a46d486458d2ee2ba
José Chio <imagentlemail@gmail.com>
2025-10-04 17:58:28 -0500

    Add library mode config

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,8 +1,13 @@
 import "lite-youtube-embed";
 import "lite-youtube-embed/src/lite-yt-embed.css";
 
-// TODO: add props type
-const LiteYoutubeEmbed = (props: unknown) => {
+interface Props {
+  videoid: string;
+  playlabel?: string;
+  title?: string;
+}
+
+const LiteYoutubeEmbed = (props: Props) => {
   // @ts-expect-error - TODO: add React intrinsic types
   return <lite-youtube {...props}></lite-youtube>;
 };

0444330c9aa3984799831ced93a8232ca03bed2a
José Chio <imagentlemail@gmail.com>
2025-10-04 18:36:08 -0500

    Create page build config

---
+++ vite.config.site.ts
@@ -0,0 +1,20 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        solo: resolve(__dirname, "/variants/solo.html"),
+      },
+    },
+  },
+});
